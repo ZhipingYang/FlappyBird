2526c07a3e419b3ab916a4f2124e8e3b714babee
Daniel Yang <daniel.yang@ringcentral.com>
2019-11-06 10:34:12 +0800

    update to swift 5.1

--- FlappyBrid/FlappyBrid/GameScene.swift
+++ FlappyBrid/FlappyBrid/GameScene.swift
@@ -15,7 +15,6 @@ struct PhysicsCatagory {
     static let Score    : UInt32 = 0x1 << 4
 }
 
-
 class GameScene: SKScene, SKPhysicsContactDelegate{
     
     var ground = SKSpriteNode()
@@ -49,11 +48,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         ground.setScale(0.5)
         ground.position = CGPoint(x: self.frame.width/2, y: ground.frame.height/2)
         
-        ground.physicsBody = SKPhysicsBody(rectangleOfSize: ground.size)
+        ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
         ground.physicsBody?.categoryBitMask = PhysicsCatagory.Ground
         ground.physicsBody?.collisionBitMask = PhysicsCatagory.Ghost
         ground.physicsBody?.contactTestBitMask = PhysicsCatagory.Ghost
-        ground.physicsBody?.dynamic = false
+        ground.physicsBody?.isDynamic = false
         ground.physicsBody?.affectedByGravity = false
         
         ground.zPosition = 3
@@ -66,11 +65,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         ghost.size = CGSize(width: 60, height: 70)
         ghost.position = CGPoint(x: self.frame.width/2 - ghost.frame.width, y: self.frame.height/2)
         
-        ghost.physicsBody = SKPhysicsBody(rectangleOfSize: ghost.size)
+        ghost.physicsBody = SKPhysicsBody(rectangleOf: ghost.size)
         ghost.physicsBody?.categoryBitMask = PhysicsCatagory.Ghost
         ghost.physicsBody?.collisionBitMask = PhysicsCatagory.Ground | PhysicsCatagory.Wall
         ghost.physicsBody?.contactTestBitMask = PhysicsCatagory.Ground | PhysicsCatagory.Wall | PhysicsCatagory.Score
-        ghost.physicsBody?.dynamic = true
+        ghost.physicsBody?.isDynamic = true
         ghost.physicsBody?.affectedByGravity = false
         
         ghost.zPosition = 2
@@ -89,18 +88,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         creatScene()
     }
     
-    override func didMoveToView(view: SKView) {
+    override func didMove(to view: SKView) {
         creatScene()
     }
     
     func creatBtn () {
         restartBtn = SKLabelNode(text: "RESTART")
-        restartBtn.color = SKColor.blueColor()
+        restartBtn.color = SKColor.blue
         restartBtn.fontSize = 50
         restartBtn.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2)
         restartBtn.zPosition = 6
         self.addChild(restartBtn)
-        restartBtn.runAction(SKAction.scaleTo(1.0, duration: 0.4))
+        restartBtn.run(SKAction.scale(to: 1.0, duration: 0.4))
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
@@ -110,7 +109,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         
         if (firstBody.categoryBitMask == PhysicsCatagory.Score && secondBody.categoryBitMask == PhysicsCatagory.Ghost)
             || (firstBody.categoryBitMask == PhysicsCatagory.Ghost && secondBody.categoryBitMask == PhysicsCatagory.Score) {
-                score++
+                score += 1
                 print("\(score)")
                 scoreLabel.text = "\(score)"
                 
@@ -129,57 +128,51 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
                 creatBtn()
             self.removeAllActions()
             
-            enumerateChildNodesWithName("wallPair", usingBlock:{
+            enumerateChildNodes(withName: "wallPair", using:{
                 (node, error) -> Void in
                 node.speed = 0
                 self.removeAllActions()
             })
         }
     }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if gameStarted == false {
-            
             gameStarted = true
-            
             ghost.physicsBody?.affectedByGravity = true
             
-            let spawn = SKAction.runBlock({
+            let spawn = SKAction.run({
                 () in
                 self.creatWalls()
             })
             
-            let delay = SKAction.waitForDuration(2.0)
+            let delay = SKAction.wait(forDuration: 2.0)
             let spawnDelay = SKAction.sequence([spawn,delay])
-            let spawnDelayForever = SKAction.repeatActionForever(spawnDelay)
-            self.runAction(spawnDelayForever)
+            let spawnDelayForever = SKAction.repeatForever(spawnDelay)
+            self.run(spawnDelayForever)
             
             let distance = CGFloat(self.frame.width + wallPair.frame.width)
-            let movePipes = SKAction.moveByX(-distance, y: 0, duration: NSTimeInterval(0.01 * distance))
+            let movePipes = SKAction.moveBy(x: -distance, y: 0, duration: TimeInterval(0.01 * distance))
             let removePipes = SKAction.removeFromParent()
             moveAndRemove = SKAction.sequence([movePipes,removePipes])
             
-            ghost.physicsBody?.velocity = CGVectorMake(0, 0)
-            ghost.physicsBody?.applyImpulse(CGVectorMake(0, 90))
+            ghost.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            ghost.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 90))
 
         } else {
             
             if died == true {
                 
             } else {
-                ghost.physicsBody?.velocity = CGVectorMake(0, 0)
-                ghost.physicsBody?.applyImpulse(CGVectorMake(0, 90))
-            
+                ghost.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                ghost.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 90))
             }
-            
         }
         
         
         for touch in touches {
-            let location = touch.locationInNode(self)
+            let location = touch.location(in: self)
             if died == true {
-                if restartBtn.containsPoint(location){
+                if restartBtn.contains(location) {
                     restartScene()
                 }
             }
@@ -194,13 +187,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         
         scoreNode.size = CGSize(width: 1, height: 200)
         scoreNode.position = CGPoint(x: self.frame.width, y: self.frame.height/2)
-        scoreNode.physicsBody = SKPhysicsBody(rectangleOfSize: scoreNode.size)
+        scoreNode.physicsBody = SKPhysicsBody(rectangleOf: scoreNode.size)
         scoreNode.physicsBody?.affectedByGravity = false
-        scoreNode.physicsBody?.dynamic = false
+        scoreNode.physicsBody?.isDynamic = false
         scoreNode.physicsBody?.categoryBitMask = PhysicsCatagory.Score
         scoreNode.physicsBody?.collisionBitMask = 0
         scoreNode.physicsBody?.contactTestBitMask = PhysicsCatagory.Ghost
-        scoreNode.color = SKColor.greenColor()
+        scoreNode.color = SKColor.green
         
         wallPair = SKNode()
         wallPair.name = "wallPair"
@@ -214,22 +207,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         topwall.setScale(0.5)
         btmwall.setScale(0.5)
         
-        topwall.physicsBody = SKPhysicsBody(rectangleOfSize: topwall.size)
+        topwall.physicsBody = SKPhysicsBody(rectangleOf: topwall.size)
         topwall.physicsBody?.categoryBitMask = PhysicsCatagory.Wall
         topwall.physicsBody?.collisionBitMask = PhysicsCatagory.Ghost
         topwall.physicsBody?.contactTestBitMask = PhysicsCatagory.Ghost
-        topwall.physicsBody?.dynamic = false
+        topwall.physicsBody?.isDynamic = false
         topwall.physicsBody?.affectedByGravity = false
         
-        btmwall.physicsBody = SKPhysicsBody(rectangleOfSize: btmwall.size)
+        btmwall.physicsBody = SKPhysicsBody(rectangleOf: btmwall.size)
         btmwall.physicsBody?.categoryBitMask = PhysicsCatagory.Wall
         btmwall.physicsBody?.collisionBitMask = PhysicsCatagory.Ghost
         btmwall.physicsBody?.contactTestBitMask = PhysicsCatagory.Ghost
-        btmwall.physicsBody?.dynamic = false
+        btmwall.physicsBody?.isDynamic = false
         btmwall.physicsBody?.affectedByGravity = false
         
-        
-        topwall.zRotation = CGFloat(M_PI)
+        topwall.zRotation = CGFloat.pi
         
         wallPair.addChild(topwall)
         wallPair.addChild(btmwall)
@@ -240,13 +232,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         wallPair.position.y = wallPair.position.y + randomPosition
         wallPair.addChild(scoreNode)
         
-        wallPair.runAction(moveAndRemove)
+        wallPair.run(moveAndRemove)
         
         self.addChild(wallPair)
     }
     
-    
-    override func update(currentTime: CFTimeInterval) {
+    override func update(_ currentTime: CFTimeInterval) {
         
     }
 }
@@ -254,15 +245,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
 
 
 public extension CGFloat{
-    
-    
-    public static func random() -> CGFloat{
-        
-        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    static func random() -> CGFloat {
+        let sum = Float(0xFFFFFFFF)
+        return CGFloat(Float(arc4random()) / sum)
     }
     
-    public static func random(min min : CGFloat, max : CGFloat) -> CGFloat{
-        
+    static func random(min : CGFloat, max : CGFloat) -> CGFloat{
         return CGFloat.random() * (max - min) + min
     }
 }
--- FlappyBrid/FlappyBrid/GameViewController.swift
+++ FlappyBrid/FlappyBrid/GameViewController.swift
@@ -10,44 +10,28 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    
+    override var shouldAutorotate: Bool { true }
+    override var prefersStatusBarHidden: Bool { return true }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
-    }
-
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
+        guard
+            let scene = GameScene(fileNamed:"GameScene"),
+            let skView = self.view as? SKView
+            else { return }
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
     }
 }
